acf653038f62bca315a0feb5c48108dacca853c3
profAndreSouza <andre.souza@sp.senai.br>
2025-05-05 21:00:29 -0300

    NLP

--- Chatbot/chatbotApp/app/nlp/semantic_analyzer.py
+++ Chatbot/chatbotApp/app/nlp/semantic_analyzer.py
@@ -1,5 +1,41 @@
-
+from app.nlp.syntatic_analyzer import SyntaticAnalyzer
+import json
+import re
 
 class SemanticAnalyzer:
-    def extract_intent_and_intentions(self, text):
-        return ["Não implementado ainda", "Não implementado ainda"]
+    def __init__(self, intents_path="app/data/intents.json", entities_path="app/data/entities.json"):
+        self.syntactic = SyntaticAnalyzer()
+        with open(intents_path, "r", encoding="utf-8") as f:
+            self.intents = json.load(f)
+        with open(entities_path, "r", encoding="utf-8") as f:
+            self.entities = json.load(f)
+
+    def extract_intent_and_entities(self, text):
+        pos_tags = self.syntactic.analyze(text)
+        intent = self.detect_intent(text, pos_tags)
+        entities = self.extract_entities(text, pos_tags)
+        return intent, entities
+
+    def detect_intent(self, text, pos_tags):
+        for intent, keywords in self.intents.items():
+            for keyword in keywords:
+                if keyword in text:
+                    return intent
+        return "intencao_desconhecida"
+
+
+    def extract_entities(self, text, pos_tags):
+        extracted = {}
+        for entity, config in self.entities.items():
+            patterns = config["patterns"]
+            group = config.get("grupo", None)
+
+            for pattern in patterns:
+                match = re.search(pattern, text)
+                if match:
+                    if group is not None:
+                        extracted[entity] = match.group(int(group))
+                    else:
+                        extracted[entity] = True
+                    break
+        return extracted
--- Chatbot/chatbotApp/main.py
+++ Chatbot/chatbotApp/main.py
@@ -1,13 +1,12 @@
 from app import create_app
 from app.nlp.text_processor import TextPreProcessor
-from app.nlp.syntatic_analyzer import SyntaticAnalyzer
 from app.nlp.semantic_analyzer import SemanticAnalyzer
 
 app = create_app()
 
 @app.route('/')
 def index():
-    perguntas_classificacao = [
+    questions = [
         "Quantas peças de cada tipo (metálicas, plásticas, refugos) são processadas por hora e qual é a taxa média de acerto da classificação?",
         "Quais são as etapas envolvidas no processo de classificação, desde a entrada da peça até sua separação final?",
         "Como o sistema identifica e diferencia materiais metálicos, plásticos e refugos? (Ex: sensores, visão computacional, inteligência artificial?)",
@@ -15,26 +14,23 @@ def index():
         "Qual é a taxa de refugo do sistema — ou seja, quantas peças são classificadas como refugo por engano ou por não atenderem aos critérios definidos?"
     ]
 
-    resposta = []
-    for enunciado in perguntas_classificacao:
+    response = []
+    for statements in questions:
 
         preprocessor = TextPreProcessor()
-        preprocessado = preprocessor.preprocess(enunciado)
-        
-        syntatic = SyntaticAnalyzer()
-        sintaxe = syntatic.analyze(preprocessado)
-
+        clean_statements = preprocessor.preprocess(statements)
+    
         semantic = SemanticAnalyzer()
-        entity, intent = semantic.extract_intent_and_intentions(enunciado)
+        intents, entities = semantic.extract_intent_and_entities(clean_statements)
 
-        resposta.append({
-            "Original": enunciado,
-            "Pré-processado": preprocessado ,
-            "Entidades": entity,
-            "Intenção": intent
+        response.append({
+            "Original": statements,
+            "Pré-processado": clean_statements ,
+            "Entidades": entities,
+            "Intenção": intents
         })
     
-    return resposta
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
